c1093c70f6d0e0fddeabdda86d5a257c2b190f39
Augusto Cesar <acs-03@hotmail.com>
2022-04-27 22:34:20 -0300

    1000

---
+++ beginner/_1000.java
@@ -0,0 +1,14 @@
+package beginner;
+
+import java.io.IOException;
+
+/**
+ * O seu primeiro programa em qualquer linguagem de programação normalmente é o "Hello World!".
+ * Neste primeiro problema tudo o que você precisa fazer é imprimir esta mensagem na tela.
+ */
+public class _1000 {
+
+  public static void main(String[] args) throws IOException {
+    System.out.println("Hello World!");
+  }
+}
