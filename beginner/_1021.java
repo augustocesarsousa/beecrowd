47bacd8f2b2a7064ceeaf68c5924a5ab89bcda60
Augusto Cesar <acs-03@hotmail.com>
2022-06-27 16:42:57 -0300

    1035

--- beginner/_1021.java
+++ beginner/_1021.java
@@ -8,25 +8,25 @@ public class _1021 {
     public static void main(String[] args) throws IOException {
         Scanner scanner = new Scanner(System.in);
 
-        int[] bills = new int[] { 100, 50, 20, 10, 5, 2 };
-        double[] coins = new double[] { 1.00, 0.50, 0.25, 0.10, 0.05, 0.01 };
-
+        double bills[] = { 100, 50, 20, 10, 5, 2 };
+        double coins[] = { 1, 0.5, 0.25, 0.1, 0.05, 0.01 };
         double amount = scanner.nextDouble();
+        int result;
 
         System.out.println("NOTAS:");
 
-        for (int bill : bills) {
-            double value = amount / bill;
-            amount = amount % bill;
-            System.out.println((int) value + " nota(s) de R$ " + bill + ".00");
+        for (double bill : bills) {
+            result = (int) (amount / bill);
+            amount %= bill;
+            System.out.printf("%d notas(s) de R$ %.2f\n", result, bill);
         }
 
         System.out.println("MOEDAS:");
 
         for (double coin : coins) {
-            double value = amount / coin;
-            amount = amount % coin;
-            System.out.println((int) value + " moeda(s) de R$ " + String.format("%.2f", coin));
+            result = (int) (amount / coin);
+            amount %= coin;
+            System.out.printf("%d moeda(s) de R$ %.2f\n", result, coin);
         }
 
         scanner.close();
---
+++ beginner/_1035.java
@@ -0,0 +1,22 @@
+package beginner;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class _1035 {
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
+        int d = scanner.nextInt();
+
+        if (b > c && d > a && (c + d) > (a + b) && c >= 0 && d >= 0 && a % 2 == 0) {
+            System.out.println("Valores aceitos");
+        } else {
+            System.out.println("Valores nao aceitos");
+        }
+
+        scanner.close();
+    }
+}
