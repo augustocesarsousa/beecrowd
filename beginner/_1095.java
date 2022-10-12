8fbaf168090e45da124e0f520014d08d70bbb87e
Augusto Cesar <acs-03@hotmail.com>
2022-10-12 19:57:35 -0300

    1095

---
+++ beginner/_1095.java
@@ -0,0 +1,12 @@
+package beginner;
+
+import java.io.IOException;
+
+public class _1095 {
+
+  public static void main(String[] args) throws IOException {
+    for (int I = 1, J = 60; J >= 0; I += 3, J -= 5) {
+      System.out.println("I=" + I + " J=" + J);
+    }
+  }
+}
