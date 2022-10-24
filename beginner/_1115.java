742ef645435a74186b39be7ebab0dcc82bed906d
Augusto Cesar <acs-03@hotmail.com>
2022-10-24 18:20:36 -0300

    1115

---
+++ beginner/_1115.java
@@ -0,0 +1,30 @@
+package beginner;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class _1115 {
+
+  public static void main(String[] args) throws IOException {
+    Scanner scanner = new Scanner(System.in);
+    int x = 0;
+    int y = 0;
+
+    while ((x = scanner.nextInt()) != 0 && (y = scanner.nextInt()) != 0) {
+      if (x > 0 && y > 0) {
+        System.out.println("primeiro");
+      }
+      if (x < 0 && y > 0) {
+        System.out.println("segundo");
+      }
+      if (x < 0 && y < 0) {
+        System.out.println("terceiro");
+      }
+      if (x > 0 && y < 0) {
+        System.out.println("terceiro");
+      }
+    }
+
+    scanner.close();
+  }
+}
