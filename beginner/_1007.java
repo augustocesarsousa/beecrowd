c336611d1be6c9846278c95771b5c71c3517d939
Augusto Cesar <acs-03@hotmail.com>
2023-01-28 15:51:46 -0300

    Adding comments

--- beginner/_1007.java
+++ beginner/_1007.java
@@ -3,6 +3,20 @@ package beginner;
 import java.io.IOException;
 import java.util.Scanner;
 
+/**
+ * Leia quatro valores inteiros A, B, C e D. A seguir, calcule e mostre a
+ * diferença do produto de A e
+ * B pelo produto de C e D segundo a fórmula: DIFERENCA = (A * B - C * D).
+ *
+ * Entrada
+ * O arquivo de entrada contém 4 valores inteiros.
+ *
+ * Saída
+ * Imprima a mensagem DIFERENCA com todas as letras maiúsculas, conforme exemplo
+ * abaixo, com um espaço
+ * em branco antes e depois da igualdade.
+ */
+
 public class _1007 {
 
   public static void main(String[] args) throws IOException {
