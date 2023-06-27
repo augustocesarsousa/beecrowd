cda2ebb173a15e51effda381a4c01b73c0723076
Augusto Cesar <acs-03@hotmail.com>
2023-06-26 22:27:51 -0300

    1478

---
+++ beginner/_1478.java
@@ -0,0 +1,43 @@
+package beginner;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+/**
+ * Escreva um algoritmo que leia um inteiro N (0 ≤ N ≤ 100), correspondente a
+ * ordem de uma matriz M de inteiros, e construa a matriz de acordo com o
+ * exemplo abaixo.
+ * 
+ * Entrada
+ * A entrada consiste de vários inteiros, um valor por linha, correspondentes as
+ * ordens das matrizes a serem construídas. O final da entrada é marcado por um
+ * valor de ordem igual a zero (0).
+ * 
+ * Saída
+ * Para cada inteiro da entrada imprima a matriz correspondente, de acordo com o
+ * exemplo. (os valores das matrizes devem ser formatados em um campo de tamanho
+ * 3 justificados à direita e separados por espaço. Após o último caractere de
+ * cada linha da matriz não deve haver espaços em branco. Após a impressão de
+ * cada matriz deve ser deixada uma linha em branco.
+ */
+public class _1478 {
+
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+
+        while (N != 0) {
+            for (int i = 1; i <= N; i++) {
+                for (int j = i; j >= 1; j--) {
+                    System.out.printf("%3d", j);
+                }
+                for (int j = 2; j <= N - i + 1; j++) {
+                    System.out.printf("%3d", j);
+                }
+                System.out.println("\n");
+            }
+        }
+        scanner.close();
+
+    }
+}
